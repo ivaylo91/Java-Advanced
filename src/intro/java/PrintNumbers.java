cc5a85651c877d42c96d702208bd3104b458f685
ivaylo91 <ipenev91@gmail.com>
2018-10-30 10:51:08 +0200

    Print Number 0 to 9

---
+++ src/intro/java/PrintNumbers.java
@@ -0,0 +1,12 @@
+package intro.java;
+
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 9; i++) {
+
+            System.out.println("Number:" + i);
+        }
+    }
+}
